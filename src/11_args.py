cd761bc4e782c00df6a58171712a262f5dace87f
avi <avkan1087@gmail.com>
2020-04-06 17:20:15 -0400

    done 1-14

--- src/11_args.py
+++ src/11_args.py
@@ -6,6 +6,11 @@
 
 # YOUR CODE HERE
 
+
+def f1(p, q):
+    return p + q
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
@@ -13,6 +18,14 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(arg1, *argv):
+    x = arg1
+    for arg in argv:
+        x = x + arg
+    return x
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -31,6 +44,11 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+
+def f3(p, q=1):
+    return p + q
+
+
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,6 +62,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for k, v in kwargs.items():
+        print('key: {}, value {}'.format(k, v))
+
 
 # Should print
 # key: a, value: 12
@@ -62,4 +84,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
